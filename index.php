9592e5e60f29ab7d73663721d35311b0ac343763
tahirmirji <tahiritguru@gmail.com>
2023-12-20 16:58:19 +0530

    forms

--- index.php
+++ index.php
@@ -1,170 +1,35 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registration Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </head>
+  <body>
+  <div class="container">
+  
+  <div class="container text-center">
+  <div class="row align-items-center">
 
-// $a=10;
-// $b=20;
-// $c= $a+$b;
+  <div class="col-2 align-items-center">
+     Left
+    </div>
 
-// echo "$a and $b = $c"."";
+    <div class="col-8 align-items-center">
+     In sunt aliqua minim sit officia duis ex. Culpa consectetur nulla magna velit tempor occaecat excepteur. Consequat irure quis et adipisicing. Sint eiusmod ex culpa anim magna non. Sit aliquip sint cupidatat et officia labore voluptate eu velit nisi. Aute sint laboris mollit in deserunt sit nulla in Lorem eiusmod fugiat.
+    </div>
 
-// define("WEBSITE_NAME","www.wiztoonz.com");
-// echo "My website name is ".WEBSITE_NAME;
+    <div class="col-2 align-items-center">
+     In Right
+    </div>
 
-// Example1
-// Local variable
-// $result=1;
-// function myFun()
-// {
-// global $result;
-// $result = 10+ 20;
-// echo "Ans: ".$result;
-// }
-// echo $result;
-// myFun();
+   
 
-//Example 2
-// $global_var ="I love wiztoonz academy";
-// function display_global()
-// {
-//     global $global_var;
-//     echo $global_var;
-// }
-// echo $global_var;
-// display_global();
-
-
-// class wiz{
-//     static $a=10;
-//     public static $b=20;
-// }
-
-// echo wiz::$a + wiz::$b;
-
-
-// $pi = 3.142;
-// $success=true;
-// $failure=false;
-// $rank=1;
-
-// if ($success)
-// {
-//     echo "Successful";
-// }
-// else if($failure){
-//     echo "Failed";
-// }
-
-// $username='Vijay';
-// $logged=true;
-// $a=10;
-// $b="Wiztoonz";
-// $c=$a+$b;
-// if($logged)
-// {
-//     print($c);
-//     print("Hey,<b> $username </b> welcome to my website");
-// }
-
-
-// function callme(&$input)
-// {
-//     $input=10;
-// }
-// $outVar=5;
-// echo $outVar;
-// echo "</br>";
-// callme($outVar);
-// echo $outVar;
-
-
-// $list = array(1,2,3,4,5);
-// $marks = array("student1"=>array(95,60,85),"student2"=>80, "student3"=>75);
-
-// echo "Student 1 taken Maths:".$marks['student1'][0].", Science:".$marks['student1'][1];
-
-// foreach( $list as $single_value)
-// {
-//     $square=$single_value*$single_value;
-
-//     echo "$square <br/>";
-    
-
-// }
-
-// $t=date("H");
-
-// if(!($t<"20"))
-// {
-//     echo "Have a good day";
-// }
-// else
-// {
-//     echo "Greetings of the day";
-// }
-
-// switch(1)
-// {
-//     case 1:
-//         echo "one number detected";
-//         break;
-
-//     case 2:
-//         echo "Second Number Detected";
-//         break;
-
-//     default:
-//     echo "Invalid input";
-//     break;
-    
-// }
-
-// $names=array('Andrew','Belen','Charli');
-// for ( $i=0 ; $i<3 ; $i++ )
-// {
-//     echo "<br/>$i. $names[$i]";
-// }
-
-// $a=1;
-// while($a<=5)
-// {
-//     echo "<br/>Count: $a";
-//     $a++; // increment a value to next value
-// }
-// echo "OUTside while";
-
-
-// $x=1;
-
-// do{
-//     echo "The number is $x <br/>";
-//     $x++;
-// } while( $x<=6 );
-
-
-
-// foreach
-
-// $colors = array('red','green','blue','yellow');
-
-// foreach($colors as $single_color)
-// {
-//     echo "Color name: $single_color <br/>";
-// }
-
-$products =array('p1' => array('Macbook m1',95000), 'p2'=>array('Macbook m2',150000) );
-foreach($products as $product)
-{
-        // 18% tax
-        $grandtotal=grand_total_calculator($product[1] ,0.28);
-    echo "Product: $product[0] | $product[1] = after 18% tax $grandtotal <br/>";
-}
-
-//User defined function
-function grand_total_calculator($price,$tax)
-{
-    $total_with_tax= ($price * $tax)+$price;
-    echo "Tax included";
-    return $total_with_tax;
-}
-
-
-?>
+  </div>
+</div>
+</div>
+   
+  </body>
+</html>
---
+++ index_1.php
@@ -0,0 +1,176 @@
+<?php
+
+// $a=10;
+// $b=20;
+// $c= $a+$b;
+
+// echo "$a and $b = $c"."";
+
+// define("WEBSITE_NAME","www.wiztoonz.com");
+// echo "My website name is ".WEBSITE_NAME;
+
+// Example1
+// Local variable
+// $result=1;
+// function myFun()
+// {
+// global $result;
+// $result = 10+ 20;
+// echo "Ans: ".$result;
+// }
+// echo $result;
+// myFun();
+
+//Example 2
+// $global_var ="I love wiztoonz academy";
+// function display_global()
+// {
+//     global $global_var;
+//     echo $global_var;
+// }
+// echo $global_var;
+// display_global();
+
+
+// class wiz{
+//     static $a=10;
+//     public static $b=20;
+// }
+
+// echo wiz::$a + wiz::$b;
+
+
+// $pi = 3.142;
+// $success=true;
+// $failure=false;
+// $rank=1;
+
+// if ($success)
+// {
+//     echo "Successful";
+// }
+// else if($failure){
+//     echo "Failed";
+// }
+
+// $username='Vijay';
+// $logged=true;
+// $a=10;
+// $b="Wiztoonz";
+// $c=$a+$b;
+// if($logged)
+// {
+//     print($c);
+//     print("Hey,<b> $username </b> welcome to my website");
+// }
+
+
+// function callme(&$input)
+// {
+//     $input=10;
+// }
+// $outVar=5;
+// echo $outVar;
+// echo "</br>";
+// callme($outVar);
+// echo $outVar;
+
+
+// $list = array(1,2,3,4,5);
+// $marks = array("student1"=>array(95,60,85),"student2"=>80, "student3"=>75);
+
+// echo "Student 1 taken Maths:".$marks['student1'][0].", Science:".$marks['student1'][1];
+
+// foreach( $list as $single_value)
+// {
+//     $square=$single_value*$single_value;
+
+//     echo "$square <br/>";
+    
+
+// }
+
+// $t=date("H");
+
+// if(!($t<"20"))
+// {
+//     echo "Have a good day";
+// }
+// else
+// {
+//     echo "Greetings of the day";
+// }
+
+// switch(1)
+// {
+//     case 1:
+//         echo "one number detected";
+//         break;
+
+//     case 2:
+//         echo "Second Number Detected";
+//         break;
+
+//     default:
+//     echo "Invalid input";
+//     break;
+    
+// }
+
+// $names=array('Andrew','Belen','Charli');
+// for ( $i=0 ; $i<3 ; $i++ )
+// {
+//     echo "<br/>$i. $names[$i]";
+// }
+
+// $a=1;
+// while($a<=5)
+// {
+//     echo "<br/>Count: $a";
+//     $a++; // increment a value to next value
+// }
+// echo "OUTside while";
+
+
+// $x=1;
+
+// do{
+//     echo "The number is $x <br/>";
+//     $x++;
+// } while( $x<=6 );
+
+
+
+// foreach
+
+// $colors = array('red','green','blue','yellow');
+
+// foreach($colors as $single_color)
+// {
+//     echo "Color name: $single_color <br/>";
+// }
+
+// $products =array('p1' => array('Macbook m1',95000), 'p2'=>array('Macbook m2',150000) );
+// foreach($products as $product)
+// {
+//         // 18% tax
+//         $grandtotal=grand_total_calculator($product[1] ,0.28);
+//     echo "Product: $product[0] | $product[1] = after 18% tax $grandtotal <br/>";
+// }
+
+// //User defined function
+// function grand_total_calculator($price,$tax)
+// {
+//     $total_with_tax= ($price * $tax)+$price;
+//     echo "Tax included";
+//     return $total_with_tax;
+// }
+
+// echo str_word_count("Hello There");
+// echo strcmp("Computer","Computer");
+// echo stripos("A B C","C");
+
+// echo strlen("Hello");
+// echo strtoupper("hello world to upper()");
+echo strtolower('HELLO');
+?>
